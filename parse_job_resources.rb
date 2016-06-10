ad4f08b1c0239287e688fb25b2e426fa5b079dc3
Stanley McMillan <stanalanmcm@gmail.com>
2016-06-10 08:06:54 -0400

    refactored to be more human friendly

--- parse_job_resources.rb
+++ parse_job_resources.rb
@@ -4,12 +4,19 @@ require 'csv'
 require 'net/http'
 
 find_url = Proc.new {|i| i[/http/]}
+file = 'job_resources_checked.csv'
+
 
 def test_url(url)
   begin
     uri = URI(url[/http.+(\/|$)/])
-    puts "Testing #{url}\n"
     @res = Net::HTTP.get_response(uri)
+    # http = Net::HTTP.new(uri.host, uri.port)
+    # http.read_timeout = 15
+    # http.open_timeout = 15
+    # @res = http.start() do |http|
+    #   http.get(url)
+    # end
     return true
   rescue
     @res = "Net:HTTP Error"
@@ -17,7 +24,7 @@ def test_url(url)
   end
 end
 
-csv = CSV.read('job_resources.csv')
+csv = CSV.read(file)
 csv.each do |arr|
   arr.compact!
 end
@@ -25,19 +32,19 @@ end
 csv.each_with_index do |row|
   next unless url = row.select(&find_url)[0]
   next if row[1] == '200' || row[1] == '302'
+  puts "Testing #{url}\n"
   test_url(url) ? res = @res.code : res = @res
   puts "Result: #{res}\n"
   row[1] = res
   if res == '301'
     redirect = @res.header['location']
-    if test_url(redirect) && @res.code == "200"
-      row[0] = redirect
-      row[1] = '200'
+    if test_url(redirect)
+      row[2] = redirect
     end
   end
 end
 
-CSV.open('job_resources_checked.csv', "wb") do |row|
+CSV.open(file, "wb") do |row|
   csv.each do |new_row|
     row << new_row
   end
