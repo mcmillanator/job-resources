a2f20b419641126154107b3c88968fd2ab01c3bb
Stanley McMillan <me@stanleymcmillan.com>
2017-11-29 07:56:44 -0500

    threaded tests

--- parse_job_resources.rb
+++ parse_job_resources.rb
@@ -6,34 +6,35 @@ require 'net/http'
 find_url = proc { |i| i[%r{http(s)?://}] }
 file = 'job_resources_checked.csv'
 
-def test_url(url)
+def test_url(url, idx)
+	@arr[idx] = Array.new
+	@arr[idx] << url
+  puts "Testing #{url}"
   uri = URI(url)
-  @res = Net::HTTP.get_response(uri)
-  return true
+  res = Net::HTTP.get_response(uri)
+	if res.code == '301'
+		redirect = res.header['location']
+		@arr[idx][2] = redirect
+	end
+	res = res.code
 rescue
-  @res = 'Net:HTTP Error'
-  return false
+	res = 'Net:HTTP Error'
+ensure
+  @arr[idx][1] = res
 end
 
 csv = CSV.read(file)
 csv.each(&:compact!)
-
-csv.each_with_index do |row|
+@arr = []
+@threads = Array.new
+csv.each_with_index do |row, idx|
   next unless (url = row.select(&find_url)[0])
-  puts "Testing #{url}\n"
-  res = test_url(url) ? @res.code : @res
-  puts "Result: #{res}\n"
-  row[1] = res
-  if res == '301'
-    redirect = @res.header['location']
-    row[2] = redirect if test_url(redirect)
-  end
+  @threads << Thread.new do
+		test_url(url, idx) 
+	#	row[1] = res
+	end
 end
-
-CSV.open(file, 'wb') do |row|
-  row << ['URLs last checked: ', Time.now]
-  csv.shift
-  csv.each do |new_row|
-    row << new_row
-  end
-end
+@threads.each(&:join)
+puts @arr
+puts csv.count
+puts @arr.count
