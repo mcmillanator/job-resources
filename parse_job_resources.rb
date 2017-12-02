3691095fad6327b9f20ae31ce7da6a2e5b0ce0bc
Stanley McMillan <me@stanleymcmillan.com>
2017-12-02 10:49:11 -0500

    thread url checking

--- parse_job_resources.rb
+++ parse_job_resources.rb
@@ -3,38 +3,64 @@ require 'pry'
 require 'csv'
 require 'net/http'
 
-find_url = proc { |i| i[%r{http(s)?://}] }
-file = 'job_resources_checked.csv'
+@arr = []
+@new_csv = Array.new
+@file = 'job_resources_checked.csv'
+@new_file = @file
+@find_url = proc { |i| i[%r{http(s)?://}] }
 
-def test_url(url, idx)
-	@arr[idx] = Array.new
-	@arr[idx] << url
+
+def test_url(url)
   puts "Testing #{url}"
   uri = URI(url)
-  res = Net::HTTP.get_response(uri)
-	if res.code == '301'
-		redirect = res.header['location']
-		@arr[idx][2] = redirect
-	end
-	res = res.code
-rescue
-	res = 'Net:HTTP Error'
+	res = Net::HTTP.get_response(uri)
+rescue => e
+	res = false 
 ensure
-  @arr[idx][1] = res
+  res
 end
 
-csv = CSV.read(file)
-csv.each(&:compact!)
-@arr = []
-@threads = Array.new
-csv.each_with_index do |row, idx|
-  next unless (url = row.select(&find_url)[0])
-  @threads << Thread.new do
-		test_url(url, idx) 
-	#	row[1] = res
+def read_csv
+	csv = CSV.read(@file)
+	csv.each(&:compact!)
+	csv.each do |row|
+		@new_csv << Thread.new do
+			url = row.select(&@find_url).first
+			if url 
+				res = test_url(url)
+				if res
+					row = [url, res.code]
+					if res.code.to_i >= 300 && res.code.to_i <= 399
+						redirect = res.header['location']	
+						row << redirect
+					end
+				else
+					row = [url, 'Failed to process url']
+				end
+			end
+			row
+		end
+	end
+	@new_csv.each(&:join)
+end
+
+def parse_results
+	arr = Array.new
+	@new_csv.shift
+	@new_csv.each do |i|
+		arr << i.value
+	end
+	arr
+end
+
+def write_csv
+	CSV.open(@new_file, 'wb') do |row|
+		row << ['URLs last checked: ', Time.now]
+		parse_results.each do |arr|
+			row << arr unless arr.nil?
+		end
 	end
 end
-@threads.each(&:join)
-puts @arr
-puts csv.count
-puts @arr.count
+
+read_csv
+write_csv
